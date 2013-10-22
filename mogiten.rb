c8a4312c64f67aef3d044bb9d8c8bf0e4cc62386
sabakan21 <ax_nightmare@yahoo.co.jp>
2013-10-22 15:58:20 +0900

    write thread programing(not run)

---
+++ mogiten.rb
@@ -0,0 +1,45 @@
+#coding: utf-8
+require 'tweetstream'
+require 'twitter'
+require '../access_token.rb'
+
+open('./random.txt')do|file|
+	$rand=file.readlines.collect{|line|line.strip}
+end
+
+TweetStream.configure do |config|
+	config.consumer_key=CONSUMER_KEY
+	config.consumer_secret=CONSUMER_SECRET
+	config.oauth_token=ACCESS_TOKEN
+	config.oauth_token_secret=ACCESS_TOKEN_SECRET
+        config.auth_method =:oauth
+end
+
+Twitter.configure do |config|
+	config.consumer_key=CONSUMER_KEY
+	config.consumer_secret=CONSUMER_SECRET
+	config.oauth_token=ACCESS_TOKEN
+	config.oauth_token_secret=ACCESS_TOKEN_SECRET
+end
+
+#userstream thread
+usrThre=Thread.new do
+	usr_client = TweetStream::Client.new
+	usr_client.userstream do |status|
+		if status.in_reply_to_screen_name==MY_NAME #同報時に取得漏れを起こす
+			tweet="@#{status.user.screen_name} #{$rand.shuffle.first}"
+			puts tweet
+		end
+	end
+end
+#pubricstream thread
+pubThre=Thread.new do
+	pub_client = TweetStream::Client.new
+	pub_client.track('#nowplaying') do |status|
+		puts status.user.screen_name+"\s"+status.text
+	#	print "\e[32m#{status.user.screen_name}\e[m" #後で調整。色変える
+	end
+end
+
+loop do
+end
